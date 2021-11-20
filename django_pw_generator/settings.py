16171599f1118ab6b9ee346374f048711fc32feb
FelixMor <fmhcde@gmail.com>
2021-11-20 01:09:09 -0500

    refactor

--- django_pw_generator/settings.py
+++ django_pw_generator/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9tpge(lagge+1#3-k+4dk1@za#v3-t^!_%hjo769wn3x%s48lx'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
