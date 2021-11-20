d5f9dda67407a7c56d8f30307de74327e4995cbd
FelixMor <fmhcde@gmail.com>
2021-11-19 20:19:37 -0500

    add 'home' route

--- django_pw_generator/urls.py
+++ django_pw_generator/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Import views
+from generator import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+
 ]
